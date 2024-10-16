45e81959473375c79c1511d29fcd3c0f3c7535d0
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2024-10-15 18:25:06 -0600

    Repositorio de Spring DWF

---
+++ sandie/src/main/java/sv/edu/udb/www/sandie/SandieApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.www.sandie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandieApplication.class, args);
+	}
+
+}
---
+++ sandie/src/test/java/sv/edu/udb/www/sandie/SandieApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.www.sandie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
